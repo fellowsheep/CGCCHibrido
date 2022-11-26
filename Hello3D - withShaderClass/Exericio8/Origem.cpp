77dc7526dcb9a75d53831ae1a202beb8871ea445
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2022-11-26 19:53:56 -0300

    Pequenos ajustes

--- Hello3D - withShaderClass/Exericio8/Origem.cpp
+++ Hello3D - withShaderClass/Exericio8/Origem.cpp
@@ -59,7 +59,7 @@ int main()
 //#endif
 
 	// Criação da janela GLFW
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola Triangulo!", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola Pirâmide!", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Fazendo o registro da função de callback para a janela GLFW
